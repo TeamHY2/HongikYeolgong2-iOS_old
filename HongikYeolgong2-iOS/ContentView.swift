2b9f87e919dee971f0ae003f45fb48d8cd871358
Seokki-Kwon <seokki0706@gmail.com>
2024-07-01 14:53:36 +0900

    Initial Commit

---
+++ HongikYeolgong2-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HongikYeolgong2-iOS
+//
+//  Created by 석기권 on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HongikYeolgong2-iOS/HongikYeolgong2_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HongikYeolgong2_iOSApp.swift
+//  HongikYeolgong2-iOS
+//
+//  Created by 석기권 on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HongikYeolgong2_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
