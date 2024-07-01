c4381a263dbef41043b23167ef5e6fb937858cc1
owllwo98 <sloopy98@naver.com>
2024-07-01 16:04:42 +0900

    feat: 초기 설정을 위해 Scene, Model, Extension 등 추가

---
+++ HongikYeolgong2-iOS/Extension/UIColor+.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+.swift
+//  HongikYeolgong2-iOS
+//
+//  Created by 변정훈 on 7/1/24.
+//
+
+import Foundation
---
+++ HongikYeolgong2-iOS/Extension/UIFont+.swift
@@ -0,0 +1,8 @@
+//
+//  UIFont+.swift
+//  HongikYeolgong2-iOS
+//
+//  Created by 변정훈 on 7/1/24.
+//
+
+import Foundation
