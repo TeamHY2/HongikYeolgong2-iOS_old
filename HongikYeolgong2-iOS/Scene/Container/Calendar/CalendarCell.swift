fbfbdadfbae1a13feb6a15ec2b02bfccf12b13c8
Seokki-Kwon <seokki0706@gmail.com>
2024-07-12 03:36:22 +0900

    feat: CalendarCell Style에 따라 Image 다르게 나오도록 설정

--- HongikYeolgong2-iOS/Scene/Container/Calendar/CalendarCell.swift
+++ HongikYeolgong2-iOS/Scene/Container/Calendar/CalendarCell.swift
@@ -7,18 +7,59 @@
 
 import SwiftUI
 
+enum CellStyle: CaseIterable {
+    case dayCount00
+    case dayCount01
+    case dayCount02
+    case dayCount03
+}
+
 struct CalendarCell: View {
     let dayOfNumber: String
+    let cellStyle: CellStyle = CellStyle.allCases.randomElement()!
     
     var body: some View {
-        VStack {
-            CustomText(font: .suite, title: dayOfNumber, textColor: .customGray300, textWeight: .medium, textSize: 14)
+        
+        switch cellStyle {
+        case .dayCount00:
+            VStack {
+                CustomText(font: .suite, title: dayOfNumber, textColor: .customGray300, textWeight: .medium, textSize: 14)
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: UIScreen.UIHeight(33))
+            .background(Color(.customGray800))
+            .cornerRadius(8)
+            .opacity(dayOfNumber.isEmpty ? 0 : 1)
+        case .dayCount01:
+            VStack {
+                CustomText(font: .suite, title: dayOfNumber, textColor: .customGray100, textWeight: .medium, textSize: 14)
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: UIScreen.UIHeight(33))
+            .background(Image(.dayCount01))
+            .cornerRadius(8)
+            .opacity(dayOfNumber.isEmpty ? 0 : 1)
+        case .dayCount02:
+            VStack {
+                CustomText(font: .suite, title: dayOfNumber, textColor: .white, textWeight: .medium, textSize: 14)
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: UIScreen.UIHeight(33))
+            .background(Image(.dayCount02))
+            .cornerRadius(8)
+            .opacity(dayOfNumber.isEmpty ? 0 : 1)
+        case .dayCount03:
+            VStack {
+                CustomText(font: .suite, title: dayOfNumber, textColor: .customGray600, textWeight: .medium, textSize: 14)
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: UIScreen.UIHeight(33))
+            .background(Image(.dayCount03))
+            .cornerRadius(8)
+            .opacity(dayOfNumber.isEmpty ? 0 : 1)
         }
-        .frame(maxWidth: .infinity)
-        .frame(height: UIScreen.UIHeight(33))
-        .background(Color(.customGray800))
-        .cornerRadius(8)
-        .opacity(dayOfNumber.isEmpty ? 0 : 1)
+        
+        
     }
 }
 
